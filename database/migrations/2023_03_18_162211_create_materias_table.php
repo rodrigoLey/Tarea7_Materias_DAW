7fbeb920e10a6846641dc47773fab991fccd6b5e
rodrigoLey <rodrigo.ley_@hotmail.com>
2023-03-18 11:42:53 -0500

    Inicio de Control de Cambios, Creacion de Modelos, Recursos y Migraciones

---
+++ database/migrations/2023_03_18_162211_create_materias_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('materias', function (Blueprint $table) {
+            $table->id();
+            $table->string('Asignatura');
+            $table->string('Actividad_1');
+            $table->integer('Calificacion_Act_1');
+            $table->string('Actividad_2');
+            $table->integer('Calificacion_Act_2');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('materias');
+    }
+};
